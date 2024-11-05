ff6667a56df8e937f1d838f5990b3e1d00f8e94d
EJSanchezC <enrique.sanchez@monetizandoideas.com>
2024-11-05 10:35:48 -0600

    Fix for inactive users

--- src/components/usersTable/uTable.jsx
+++ src/components/usersTable/uTable.jsx
@@ -104,6 +104,8 @@ const UsersTable = () => {
         const organization = parsedPermissions.Organization;
         api.post('/user/users/fetchIns', { organization })
             .then((response) => {
+                console.log(response)
+                console.log(response.data)
                 const fetchedData = response.data.data.map(item => {
                     let role;
                     switch (item.role_id) {
