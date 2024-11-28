a52c60b68ec26506d1cdf05f4d5fea4f84a944fb
EJSanchezC <enrique.sanchez@monetizandoideas.com>
2024-11-28 11:08:02 -0600

    Fix on set days

--- src/components/misc/sideBar.tsx
+++ src/components/misc/sideBar.tsx
@@ -108,7 +108,7 @@ export default function Sidebar() {
                     </div>
                 )}
                 <div className="absolute bottom-2 left-2 text-xs text-gray-400">
-                    V 2.6.8
+                    V 2.6.9
                 </div>
             </div>
         </div>
