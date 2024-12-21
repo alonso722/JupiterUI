8c9d2de0fa4608124ce5d160dafb1acaff6d7ab8
EJSanchezC <enrique.sanchez@monetizandoideas.com>
2024-12-20 18:59:42 -0600

    Stash for reports

--- src/components/reports/reportsMenu.jsx
+++ src/components/reports/reportsMenu.jsx
@@ -1,48 +1,150 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, Fragment  } from "react";
 import { FaUserCheck, FaCalendarCheck } from "react-icons/fa6";
 import useApi from "@/hooks/useApi";
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 import { useColors } from "@/services/colorService";
 
-export const ReportsMenu = ({  }) => {
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
+
+export const ReportsMenu = () => {
     const [permissions, setPermissions] = useState([]);
-    const [selectedButton, setSelectedButton] = useState(0);
+    const [selectedButton, setSelectedButton] = useState(null);
+    const [departments, setDepartments] = useState([]);
+    const [data, setData] = useState(null);
     const { primary, secondary } = useColors();
-    const effectMounted = useRef(false);
+    const [selectedDepartments, setSelectedDepartments] = useState([]);
+    const effectMounted = useRef(false); 
     const api = useApi();
 
     useEffect(() => {
-        if (effectMounted.current === false) { 
+        if (!effectMounted.current) {
             let parsedPermissions;
-            const storedPermissions = localStorage.getItem("permissions");
-            const token = localStorage.getItem("token");
+            const storedPermissions = localStorage.getItem('permissions'); 
             if (storedPermissions) {
                 parsedPermissions = JSON.parse(storedPermissions);
-                setPermissions(parsedPermissions);
             }
-            const uuid = parsedPermissions?.uuid;
-            if (uuid) {
-                api.post("/user/reports/getDaily", { uuid })
-                    .then((response) => {
-                        const uName = response.data.name;
-                        const uLast = response.data.last;
-                        setName(uName);
-                        setLast(uLast);
-                    })
-                    .catch((error) => {
-                        console.error("Error al consultar nombre:", error);
-                    });
-            }
+            const organization= parsedPermissions.Organization;
+            api.post('/user/departments/fetch', { organization })
+            .then((response) => {
+                const departments = response.data.data;
+                const filteredDepartments = departments.filter(dept => dept.type !== 41);
+                setDepartments(filteredDepartments);
+                const fetchedData = filteredDepartments.map(item => ({
+                    id: item.id,
+                    department: item.department
+                }));
+                console.log(fetchedData)
+                setDepartments(fetchedData);
+            })
+            .catch((error) => {
+                console.error("Error al consultar departamentos:", error);
+            });
             effectMounted.current = true;
         }
     }, []);
 
+    const handleSelectionDepartment = (selectedDepartment) => {
+        setSelectedDepartments([selectedDepartment]);
+      };
+
+    const fetchData = async (index) => {
+        let reque = {};
+        const storedPermissions = localStorage.getItem("permissions");
+        const parsedPermissions = storedPermissions ? JSON.parse(storedPermissions) : null;
+        const orga = parsedPermissions?.Organization;
+
+        reque.orga = orga;
+        reque.department = selectedDepartments[0]?.id;
+        if (!orga) return;
+
+        const endpoints = [
+            "/user/reports/getDaily", 
+            "/user/reports/getDaily",
+        ];
+
+        const endpoint = endpoints[index];
+
+        try {
+            console.log("Haciendo consulta...", reque);
+            const response = await api.post(endpoint, reque);
+            setData(response.data);
+            console.log(`Datos obtenidos de ${endpoint}:`, response.data);
+        } catch (error) {
+            console.error(`Error al consultar ${endpoint}:`, error);
+        }
+    };
+
     const handleButtonClick = (index) => {
         setSelectedButton(index); 
+        fetchData(index);        
     };
 
     return (
         <div className="mt-[80px] ml-[110px] mr-[0px] text-neutral-50 rounded overflow-hidden">
             <p className="text-[25px] text-black my-4"><b>Reportes</b></p>
+            <Listbox value={selectedDepartments} onChange={handleSelectionDepartment} className="max-w-[100px] mb-4">
+            {({ open }) => (
+              <>
+                <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Departamento</Listbox.Label>
+                <div className="relative mt-2">
+                  <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6 max-w-[150px]">
+                    <span className="flex items-center">
+                      <span className="ml-3 block truncate">
+                        {selectedDepartments.length > 0 ? selectedDepartments[0].department : "Seleccionar departamento"}
+                      </span>
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                      <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0">
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {departments.map((department) => (
+                        <Listbox.Option
+                          key={department.id}
+                          className={({ active }) =>
+                            classNames(
+                              active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                              'relative cursor-default select-none py-2 pl-3 pr-9'
+                            )
+                          }
+                          value={department}>
+                          {({ selected, active }) => (
+                            <>
+                              <div className="flex items-center">
+                                <span
+                                  className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}>
+                                  {department.department}
+                                </span>
+                              </div>
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    active ? 'text-white' : 'text-indigo-600',
+                                    'absolute inset-y-0 right-0 flex items-center pr-4'
+                                  )}>
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
             <div className="flex w-full pl-5 mt-9">
                 <div className="flex flex-col items-center justify-center mr-5">
                     <button
@@ -57,7 +159,7 @@ export const ReportsMenu = ({  }) => {
                             style={{ color: selectedButton === 0 ? secondary : primary, width: "25px", height: "28px" }}
                         />
                     </button>
-                    <p className="text-black">Registros de entrada</p>
+                    <p className="text-black">Asistencia</p>
                 </div>
                 <div className="flex flex-col items-center justify-center mx-5">
                     <button
@@ -74,6 +176,13 @@ export const ReportsMenu = ({  }) => {
                     </button>
                     <p className="text-black">Vacaciones</p>
                 </div>
+            </div>
+            <div className="mt-5 p-4 bg-gray-100 rounded">
+                {data ? (
+                    <pre className="text-black">{JSON.stringify(data, null, 2)}</pre>
+                ) : (
+                    <p className="text-black">Selecciona un reporte para ver los datos</p>
+                )}
             </div>
         </div>
     );
